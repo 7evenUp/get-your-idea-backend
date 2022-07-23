05b3e8707fc714135ef54998ff0795a132869ee2
Artyom <sheludeshev.artyom@mail.ru>
2022-07-23 23:17:45 +0300

    Добавил POST метод

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Request } from 'express'
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  getTextInput(@Req() request: Request): string {
+    console.log("GOT POST REQUEST")
+    console.log("BODY: ", request.body)
+    return 'Server received message'
   }
 }
